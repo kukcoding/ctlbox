bfa926767fdfa63f84586fd5d269e968b51eb0d1
jjfive <softdada@gmail.com>
2021-11-08 07:57:41 +0900

    update minor

--- _build-apk-prod-debug.sh
+++ _build-apk-prod-debug.sh
@@ -4,4 +4,4 @@ set -e
 # ./gradlew :app:assembleProdDebug
 ./gradlew :app:assembleDevDebug
 
-#scp app/build/outputs/apk/dev/debug/*.apk app@192.168.114.3:/home/www/html/p/kuk/apks/
+scp app/build/outputs/apk/dev/debug/*.apk app@192.168.114.3:/home/www/html/p/kuk/apks/
